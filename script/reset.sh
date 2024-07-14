5293145c6ece81145e60be2bb5f4a7be4c2aaccd
Richard Kielty <rich.kielty@gmail.com>
2024-07-14 08:06:58 +0100

    ci: add release dry run to post-merge checks

---
+++ script/reset.sh
@@ -0,0 +1,8 @@
+
+# Remove all the files and directories and reset the repository.
+rm -rf ./.nx/cache/
+rm -rf ./.nx/installation/
+rm -rf ./.nx/workspace-data/
+rm -rf ./artifact/
+rm -rf ./coverage/
+rm -rf ./tmp/
